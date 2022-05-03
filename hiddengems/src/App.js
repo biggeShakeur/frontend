75470e26b47cb9b9b33e06384bc9a39df3ce5416
Anthony Hill <anthonymhill23@gmail.com>
2022-05-03 17:47:22 -0400

    added auth0 for testing

--- hiddengems/src/App.js
+++ hiddengems/src/App.js
@@ -1,2 +1,63 @@
 import React from "react";
-import Header from './Header'
+import Header from "./Header";
+import Footer from "./Footer.js";
+import Login from "./Login";
+import Profile from "./Profile";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { withAuth0 } from '@auth0/auth0-react';
+import Trip from "./Trip";
+
+
+
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      email:'',
+    }
+  }
+
+  loginHandler = (user, email) => {
+    console.log(user, email)
+    this.setState({
+      user: user,
+      email: email,
+    })
+  }
+
+  logoutHandler = () => {
+    this.setState({
+      user: '',
+    })
+  }
+
+  
+  render() {
+    return (
+      <>
+        <Router>
+          <Header user={this.state.user} onLogout={this.logoutHandler} />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              {this.props.auth0.isAuthenticated ? <Trip/> : <Login loginHandler={this.loginHandler}/>}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route path = "/Profile">
+              {this.props.auth0.isAuthenticated ? <Profile/>: null}
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    )
+  }
+}
+
+export default withAuth0(App);
