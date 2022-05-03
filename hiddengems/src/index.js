e96feb36957c9ded3c3a4fdff83a7996a3ca0d63
Anthony Hill <anthonymhill23@gmail.com>
2022-05-03 18:42:24 -0400

    react update

--- hiddengems/src/index.js
+++ hiddengems/src/index.js
@@ -3,14 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(
+root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById("root")
+  </Auth0Provider>
+
 );
