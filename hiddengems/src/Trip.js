c8155157e067aaf579b3e6ebd9b801a374cf89d2
Anthony Hill <anthonymhill23@gmail.com>
2022-05-03 18:18:24 -0400

    minor code addition

--- hiddengems/src/Trip.js
+++ hiddengems/src/Trip.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Button, Modal } from "react-boostrap";
