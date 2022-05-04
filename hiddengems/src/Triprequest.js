3699aae9dae49f26e159062e4c749f9aefd2734f
Anthony Hill <anthonymhill23@gmail.com>
2022-05-03 21:48:23 -0400

    code pushup for team

--- hiddengems/src/Triprequest.js
+++ hiddengems/src/Triprequest.js
@@ -1,15 +1,7 @@
 import React from "react";
 import axios from 'axios';
 import { withAuth0 } from 'auth0-react';
-
-
-
-
-
-
-
-
-
+import { Carousel } from "react-bootstrap";
 
 class Triprequest extends React.Component {
   constructor(props) {
@@ -19,7 +11,6 @@ class Triprequest extends React.Component {
       tripformModal: false,
     }
   }
-
 
   fetchTrip = async () => {
 
@@ -33,7 +24,7 @@ class Triprequest extends React.Component {
       const response = await axios.get(url);
       console.log(response);
       this.setState({
-        triprequest: response.data
+        Triprequest: response.data
       })
       console.log(this.state.triprequest);
     } catch (error) {
@@ -43,30 +34,84 @@ class Triprequest extends React.Component {
 
   }
 
-
   deleteTrip = async (id) => {
     console.log(id);
     try {
       await axios.delete(`${process.env.REACT_APP_SERVER}/trip/${id}`);
       let updatedTrip = this.state.triprequest.filter(triprequest => triprequest._id !== id);
       this.setState({
-        triprequest: updatedTrip
+        Triprequest: updatedTrip
       })
     } catch (error) {
-      console.log('Error at deleteTrip Line 47',error.response.data);
+      console.log('Error at deleteTrip Line 47', error.response.data);
     }
 
   }
 
-
-
   addTrip = async (newTrip) => {
     try {
       let newTrip = await axios.put(`${process.env.REACT_APP_SERVER}/trip/`, newTrip)
-
+      this.setState({
+        Triprequest: [...this.state.Triprequest, newTrip.data]
+      })
 
     } catch (error) {
       console.log('Error at addTrip Line 64: ', error.response.data);
     }
   }
+
+
+  updateTrip = async (triptoUpdate) => {
+    try {
+      let freshTrip = await axios.put(`${process.env.REACT_APP_SERVER}/trip/${triptoUpdate._id}`, triptoUpdate);
+      let updatedTripdata = this.state.triprequest.map(currentTrip => {
+        return currentTrip._id === triptoUpdate._id ? freshTrip.data : currentTrip;
+      })
+      this.setState({ Triprequest: updatedTripdata })
+    } catch (error) {
+      console.log(error.message);
+
+
+
+    }
+
+    showForm = () =>
+      this.setState({
+        tripformModal: true
+
+      })
+
+  }
+  onHide = () => {
+    this.setState({
+      tripformModal: true
+
+    })
+
+
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.Triprequest.length ? (
+          this.state.Triprequest.map((trip => {
+            <Carousel.Item key={trip._id}>
+              <Trip
+                deleteTrip={this.deleteTrip}
+                updateTrip={this.updateTrip}
+              />
+            </Carousel.Item>
+          }))
+        )
+          :
+          (
+            <h3>No trips found</h3>
+          )}
+
+        )
+      </>
+    )
+  }
 }
+export default withAuth0(Triprequest);
--- hiddengems/src/index.js
+++ hiddengems/src/index.js
@@ -1,18 +1,19 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
-const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container);
 
-root.render(
+const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
+
+ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
   >
     <App />
-  </Auth0Provider>
+  </Auth0Provider>,
+  container
 
 );
